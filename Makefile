4b86ad093e1036d08195187941bf59f96568aaeb
paulhjwu <paulhjwu@gmail.com>
2023-12-27 23:05:57 +0800

    Added headless selenium testeing

---
+++ Makefile
@@ -0,0 +1,26 @@
+# install:
+# 	pip install --upgrade pip &&\
+# 		pip install -r requirements.txt
+
+# lint:
+# 	pylint --disable=R,C *.py devopslib
+
+test:
+	python -m pytest -vv 
+#   --cov=devopslib test_*.py
+
+format:
+	black *.py devopslib/*.py
+
+# post-install:
+# 	python -m textblob.download_corpora
+
+# deploy:
+# 	#deploy
+# 	aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 561744971673.dkr.ecr.us-east-1.amazonaws.com
+# 	docker build -t devops-may-2022 .
+# 	docker tag devops-may-2022:latest 561744971673.dkr.ecr.us-east-1.amazonaws.com/devops-may-2022:latest
+# 	docker push 561744971673.dkr.ecr.us-east-1.amazonaws.com/devops-may-2022:latest
+
+# all: install post-install lint test format deploy
+all: test format 
--- tests/selenium/test_booking.py
+++ tests/selenium/test_booking.py
@@ -1,17 +1,39 @@
 # Must consult the table in https://firefox-source-docs.mozilla.org/testing/geckodriver/Support.html 
 # to identify compatible versions of geckodriver and Firefox
 # In my wsl, I use the top line of the table, which is geckodriver v0.33.0 and Firefox > 102 ESR
-# export DISPLAY=ip_address:0.0
+
+# Ref: https://www.rickmakes.com/windows-subsystem-for-linux-2-installing-vcxrv-x-server/
+# Add the following to ~/.bashrc
+# export DISPLAY=172.18.144.1:0.0
 # export LIBGL_ALWAYS_INDIRECT=1
+# also need to set DNS in /etc/resolv.conf
+# echo -e "nameserver 172.18.144.1\nnameserver 8.8.8.8\nnameserver 8.8.4.4" | sudo tee /etc/resolv.conf
+# where 172.18.144.1 is the ip address of the WSL host by doing ipconfig in cmd.exe
+
+# Also to make XcXsrv X server start automatically 
+# by moving config file /d/tars/config.xlaunch to %APPDATA%\roaming\Microsoft\Windows\Start Menu\Programs\Startup 
+# config.xlaunch is configured by starting Xlaunch and saving the config file
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 import time
+import os
 
 def test_booking():
     service_obj = Service("/d/tars/geckodriver")
-    driver = webdriver.Firefox(service=service_obj)
+
+    # added for headless 
+    options = FirefoxOptions()
+    # options.headless = True
+
+    if os.getenv('MOZ_HEADLESS') == '1':
+        options.add_argument("--headless")  
+        options.add_argument("--window-size=1920x1080") 
+
+    driver = webdriver.Firefox(service=service_obj, options=options)
+
     driver.maximize_window()
     driver.get("localhost:5000")
 
